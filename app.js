6a200b3312aa62dacfb0aa22d3eb08534a7bb495
Astro <astro@spaceboyz.net>
2013-06-19 01:31:45 +0200

    LoadController: replace file-receiver with filesystem.chooseEntry

--- app.js
+++ app.js
@@ -2,23 +2,6 @@
 
 var app = angular.module('Bitford', []);
 
-app.directive('fileReceiver', function() {
-    return {
-	restrict: 'A',
-	link: function($scope, element, attrs) {
-	    element.bind('change', function(ev) {
-            var el = ev.target || ev.srcElement;
-            var files = el.files;
-            $scope.$apply(function() {
-                var cb = $scope[attrs['fileReceiver']];
-                for(var i = 0; i < files.length; i++)
-                    cb(ev.target.files.item(i));
-                });
-            });
-        }
-    };
-});
-
 app.service('Torrents', function() {
     return [];
 });
@@ -26,22 +9,33 @@ app.service('Torrents', function() {
 app.controller('LoadController', function($scope, Torrents) {
     $scope.torrents = [];
     $scope.loadFile = function(file) {
-	var reader = new FileReader();
-	reader.onload = function() {
-	    // TODO: handle load & parse errors?
-	    var torrentMeta = BEnc.parse(reader.result);
-	    console.log("meta", torrentMeta);
-	    /* Calc infoHash */
-	    var sha1 = new Digest.SHA1();
-	    var infoParts = BEnc.encodeParts(torrentMeta.info);
-	    infoParts.forEach(sha1.update.bind(sha1));
-	    torrentMeta.infoHash = new Uint8Array(sha1.finalize());
+	chrome.fileSystem.chooseEntry({
+	    type: 'openFile',
+	    accepts: [{
+		description: "BitTorrent metainfo file",
+		mimeTypes: ["application/x-bittorrent"],
+		extensions: ["torrent"]
+	    }]
+	}, function(entry) {
+	    entry.file(function(file) {
+		var reader = new FileReader();
+		reader.onload = function() {
+		    // TODO: handle load & parse errors?
+		    var torrentMeta = BEnc.parse(reader.result);
+		    console.log("meta", torrentMeta);
+		    /* Calc infoHash */
+		    var sha1 = new Digest.SHA1();
+		    var infoParts = BEnc.encodeParts(torrentMeta.info);
+		    infoParts.forEach(sha1.update.bind(sha1));
+		    torrentMeta.infoHash = new Uint8Array(sha1.finalize());
 
-	    var torrent = new Torrent(torrentMeta);
-	    // TODO: infoHash collision?
-	    Torrents.push(torrent);
-	};
-	reader.readAsArrayBuffer(file);
+		    var torrent = new Torrent(torrentMeta);
+		    // TODO: infoHash collision?
+		    Torrents.push(torrent);
+		};
+		reader.readAsArrayBuffer(file);
+	    });
+	});
     };
 });
 
--- background.js
+++ background.js
@@ -1,13 +1,4 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-    chrome.fileSystem.chooseEntry({
-	type: 'openFile',
-	accepts: [{
-	    description: "BitTorrent metainfo file",
-	    mimeTypes: ["application/x-bittorrent"],
-	    extensions: ["torrent"]
-	}]
-    }, function() {
-    });
     chrome.app.window.create('main.html', {
 	id: "main",
 	bounds: {
